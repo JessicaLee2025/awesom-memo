5f10521ecc4a54a35fb5fe5f697ed1a779d807c2
JessicaLee2025 <darohull1822@gmail.com>
2025-05-07 12:19:51 -0400

    최종수정
    최종수정하였습니다.

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 class Memo(BaseModel):
-  id:str
-  content:str
-  
-memos = []
+    id:str
+    content:str
+
+memos=[]
 
 @app.post("/memos")
-def create_memo(memo:Memo):
-    memos.append(memo)
-    return {"message": "메모 추가에 성공했음!"}
-  
-@app.get("/memos")  
+def create_memo(memo: Memo):
+    memos.append(memo) 
+    return {"message": "메모 추가 성공"}
+
+@app.get("/memos")
 def read_memo():
-  return memos
-  
+    return memos
+
 @app.put("/memos/{id}")
 def put_memo(req_memo:Memo):
-  for memo in memos:
-    if memo.id== req_memo.id:
-      memo.content=req_memo.content
-      return "성공"
-  return "그런건 없습니다!"
+    for memo in memos:
+        if memo.id==req_memo.id:
+            memo.content=req_memo.content
+            return '성공'
+    return '존재하지 않는 메모입니다.'
 
 @app.delete("/memos/{memo_id}")
 def delete_memo(memo_id:str):
-  for index,memo in enumerate(memos):
-    if memo.id== memo.id:
-       memos.pop(index)
-       return "성공"
-  return "그런건 없습니다!"
+    for index,memo in enumerate(memos):
+        if memo.id==memo_id:
+            memos.pop(index)
+            return '성공'
+    return '존재하지 않는 메모입니다.'
 
-app.mount("/", StaticFiles(directory="static", html=True),name="static")
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
